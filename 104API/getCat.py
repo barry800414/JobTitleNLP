3ec7160430a315c4c9f963fba49e28c279429647
Wei-Ming <barry800414@gmail.com>
2016-09-07 10:11:29 +0800

    add functions

--- 104API/getCat.py
+++ 104API/getCat.py
@@ -14,7 +14,22 @@ def getL3ID(rawCat):
 			for L3 in L2['n']:
 				d[L3['no']] = L3['des']
 	return d
-	
+
+def getL3ToL2(rawCat):
+	to2 = dict()
+	for L1 in rawCat['n']:
+		for L2 in L1['n']:
+			for L3 in L2['n']:
+				to2[L3['des']] = L2['des']
+	return to2
+
+def getL2ToL1(rawCat):
+	to1 = dict()
+	for L1 in rawCat['n']:
+		for L2 in L1['n']:
+			to1[L2['des']] = L1['des']
+	return to1
+
 if __name__ == '__main__':
 	r = requests.get('http://www.104.com.tw/104i/js/js.cfm?p=documentation.cfm') 
 	print('Status:', r.status_code)
