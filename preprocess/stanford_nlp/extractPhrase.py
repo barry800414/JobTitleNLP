3dd7cb3fe8ab2476603a01f89d8de1a79e25df64
Wei-Ming <barry800414@gmail.com>
2016-09-08 14:06:59 +0800

    extract phrases from constituent parsing

--- preprocess/stanford_nlp/extractPhrase.py
+++ preprocess/stanford_nlp/extractPhrase.py
@@ -1,63 +1,64 @@
 import sys, json
-
-class Node:
-	def __init__(self, _id, _type, tagWord):
-		self.children = None
-		self._id = _id
-		self._type = _type
-		self.tagWord = tagWord
-
-	# assume node id is in ascending order
-	def addChild(self, node):
-		if self.child is None:
-			self.children = list()
-		self.child.append(node)
+import networkx as nx
 
 
-class Tree:
-	def __init__(self):
-		self.nodes = dict()
+def getOriginalString(tree):
+	words = [node[1]['tagWord'] for node in tree.nodes(data=True) if node[1]['_type'] == 'word']
+	return ' '.join(words)
 
-	def setRoot(self, root):
-		self.root = root
+def getPhrases(tree, tagSet=set(['NP'])):
+	calcLayer(tree)
+	candidate = [n[0] for n in tree.nodes(data=True) if (n[1]['tagWord'] in tagSet and n[1]['_type'] == 'phrase')]
+	phrases = list()
+	for nodeID in candidate:
+		if tree.node[nodeID]['layer'] == 2:
+			phrases.append(getString(tree, nodeID))
+	return phrases
 
-	def addNode(self, node):
-		assert node._id not in self.nodes
-		self.nodes[node._id] = node
-	
-	def addEdge(self, parentID, childID):
-		assert parentID in self.nodes
-		assert childID in self.nodes
-		self.nodes[parentID].addChild(self.nodes[childID])
+def getString(tree, nodeID):
+	return __traverse(tree, nodeID, '')
 
-	def getOriginalString(self):
-		words = [node.tagWord for node in self.nodes.values() if node._type == 'word']
-		return ' '.join(words)
+def __traverse(tree, nodeID, string):
+	if tree.out_degree(nodeID) == 0:
+		if tree.node[nodeID]['_type'] == 'word':
+			string = string + tree.node[nodeID]['tagWord']
+		return string
+	else:
+		for nextID in sorted(tree.neighbors(nodeID)):
+			string = __traverse(tree, nextID, string)
+		return string
 
-	def traverse(self):
-		pass		
+def calcLayer(tree):
+	_calcLayer(tree, 0)
 
-	def extractPhrase(self):
-		pass
-
+def _calcLayer(tree, nodeID):
+	if tree.out_degree(nodeID) == 0:
+		tree.node[nodeID]['layer'] = 0
+		return 0
+	else:
+		layer = -1
+		for nextID in tree.neighbors(nodeID):
+			r = _calcLayer(tree, nextID) + 1
+			if r > layer:
+				layer = r
+		tree.node[nodeID]['layer'] = layer
+		return layer
 
 class TreeFactory:
 	def build(nodesList, edgesList):
-		tree = Tree()
-		for node in nodesList:
-			s = node.split(' ')
+		tree = nx.DiGraph()
+		for nStr in nodesList:
+			s = nStr.split(' ')
 			assert len(s) == 3
 			_id, _type, tagWord = int(s[0]), s[1], s[2]
-			node = Node(_id, _type, tagWord)
-			tree.addNode(node)
-			if _id == 0:
-				tree.setRoot(node)
+			tree.add_node(_id, _type=_type, tagWord=tagWord)
 
 		for edge in edgesList:
-			s = node.split(' ')
+			s = edge.split(' ')
 			assert len(s) == 2
 			parentID, childID = int(s[0]), int(s[1])
-			tree.addEdge(parentID, childID)
+			tree.add_edge(parentID, childID)
+
 		return tree
 
 
@@ -69,12 +70,19 @@ if __name__ == '__main__':
 	with open(sys.argv[1], 'r') as f:
 		data = json.load(f)
 
+	newData = dict()
 	for jobCat, titles in data.items():
+		newData[jobCat] = list()
 		for title in titles:
-			tree = TreeFactory.build(title)
-			print(tree.getOriginalString())
-			break
+			r = dict()
+			tree = TreeFactory.build(title[0], title[1])
+			r['original'] = getOriginalString(tree)
+			r['phrases'] = getPhrases(tree)
+			newData[jobCat].append(r)
 		break
+
+	with open(sys.argv[2], 'w') as f:
+		json.dump(newData, f, indent=1, ensure_ascii=False)
 
 
 
