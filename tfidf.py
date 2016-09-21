0df6af22922d4a724b14dc95219b017a45605872
Wei-Ming <barry800414@gmail.com>
2016-09-21 14:33:38 +0800

    update tfidf

--- tfidf.py
+++ tfidf.py
@@ -2,10 +2,21 @@
 import sys, json, math
 from collections import defaultdict
 
+'''
+To calculate TF-IDF value for each job-title term (in window-size) in each job category
+and to sort the list of term by TF-IDF value
+
+Each job category is viewed as a document in TF-IDF calculation
+Each job title is like a sentence in the document
+'''
+
+# data: job-category -> list of job-titles
+# dim-list: the list of window size 
 def calcTFIDF(data, dim_list=[1,2,3,4,5]):
     result = dict()
 
     # calculate idf first
+    print('Calculating idf ...', file=sys.stderr)
     idf = defaultdict(float)
     for jobCat, titles in data.items():
         for dim in dim_list:
@@ -23,7 +34,9 @@ def calcTFIDF(data, dim_list=[1,2,3,4,5]):
         idf[key] = math.log(len(data) / value)
     
     # calculate tf
-    for jobCat, titles in data.items():
+    print('Calculating tf ...', file=sys.stderr)
+    for i, (jobCat, titles) in enumerate(data.items()):
+        print('Progress: (%d/%d) %s' % (i, len(data), jobCat), file=sys.stderr)
         tf = defaultdict(float)
         for dim in dim_list:
             for doc in titles:
@@ -42,7 +55,6 @@ def calcTFIDF(data, dim_list=[1,2,3,4,5]):
         # finally calculate idf
         tfidf = { k: v*idf[k] for k, v in tf.items() }
 
-        print('==================category : %s===================' % jobCat)
         result[jobCat] = list()
         for key, value in sorted(list(tfidf.items()), key=lambda x:x[1], reverse=True):
             result[jobCat].append([''.join(key), value])
@@ -50,8 +62,8 @@ def calcTFIDF(data, dim_list=[1,2,3,4,5]):
     return result
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Usage:', sys.argv[0], 'inJsonFile', file=sys.stderr)
+    if len(sys.argv) != 3:
+        print('Usage:', sys.argv[0], 'Cat2TitleJsonFile outTFIDFJsonFile', file=sys.stderr)
         exit(-1)
 
     with open(sys.argv[1], 'r') as f:
@@ -59,6 +71,6 @@ if __name__ == '__main__':
 
     result = calcTFIDF(data)
 
-    with open('test.json', 'w') as f:
+    with open(sys.argv[2], 'w') as f:
         json.dump(result, f, indent=1, ensure_ascii=False)
 
