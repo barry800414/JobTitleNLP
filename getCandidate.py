4af9faded8679414d84287c9487b3e530ee2b994
Wei-Ming <barry800414@gmail.com>
2016-09-21 16:42:49 +0800

    add topPercent min max

--- getCandidate.py
+++ getCandidate.py
@@ -1,32 +1,61 @@
-import json
+import sys, json
 
-with open('tfidf.json', 'r') as f:
-	tfidf = json.load(f)
 
-with open('data.json', 'r') as f:
-	data = json.load(f)
+'''
+websiteData: data crawled from our website
+apiData: data crawled from 104 API (preprocessed and filtered)
+'''
 
-titleSet = set([d['job_title'] for d in data])
+def parseArgument(argv):
+	top = float(argv[5]) if len(argv) >=6 else 0.05
+	minSize = int(argv[6]) if len(argv) >=7 else None
+	maxSize = int(argv[7]) if len(argv) >=8 else None
+	return (top, minSize, maxSize)
+ 
+def getCandidateNum(total, top, minSize, maxSize):
+	num = round(top*total)
+	if minSize is not None:
+		if num < minSize:
+			num = minSize
+	if maxSize is not None:
+		if num > maxSize:
+			num = maxSize
+	return num
+		
+if __name__ == '__main__':
+	if len(sys.argv) < 5: 
+		print('Usage:', sys.argv[0], 'webSiteData apiData outJson outTxt [[[topPercent] min] max]', file=sys.stderr)
+		exit(-1)
 
-top = 0.05
+	with open(sys.argv[1], 'r') as f:
+		data = json.load(f)
+	titleSet = set([d['job_title'] for d in data])
 
-newList = dict()
-for jobCat, titleList in tfidf.items():
-	tmp = list()
-	num = round(top*len(titleList))
-	for i, (title, value) in enumerate(titleList):
-		if title in titleSet:
-			tmp.append([title, 1])
-		elif i < num:
-			tmp.append([title, value])
-	tmp.sort(key=lambda x:x[1], reverse=True)
-	newList[jobCat] = tmp
-	print(jobCat, len(tmp))
+	with open(sys.argv[2], 'r') as f:
+		tfidf = json.load(f)
 
-with open('test.json', 'w') as f:
-	json.dump(newList, f, indent=1, ensure_ascii=False)
+	top, minSize, maxSize = parseArgument(sys.argv)
 
-for jobCat, titleList in newList.items():
-	print('-----------------Category:%s------------------' % (jobCat))
-	for key, value in titleList:
-		print(key, value)
+	newDict = dict()
+	for i, (jobCat, titleList) in enumerate(tfidf.items()):
+		newList = list()
+		num = getCandidateNum(len(titleList), top, minSize, maxSize)
+		print('Progress (%d/%d) %s targetNum: %d' % (i, len(tfidf), jobCat, num), file=sys.stderr)
+		for title, value in titleList:
+			if title in titleSet:
+				newList.append([title, 1])
+			else: 
+				newList.append([title, value])
+			if len(newList) >= num:
+				break
+		newList.sort(key=lambda x:x[1], reverse=True)
+		newDict[jobCat] = newList
+
+	with open(sys.argv[3], 'w') as f:
+		json.dump(newDict, f, indent=1, ensure_ascii=False)
+
+	with open(sys.argv[4], 'w') as f:
+		for jobCat, titleList in newDict.items():
+			print('----------%s----------' % (jobCat), file=f)
+			for key, value in titleList:
+				print(key, value, file=f)
